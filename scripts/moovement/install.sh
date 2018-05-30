ff4103e78907ff899524f293ef19d0fa87548c3c
Anton Perez <aperez@mobiquityinc.com>
2018-05-30 13:41:16 +0200

    install aws cli tools (serverless + awscli)
    Signed-off-by: Daniel Garnier-Moiroux <dgarnier-moiroux@pivotal.io>

--- scripts/moovement/install.sh
+++ scripts/moovement/install.sh
@@ -5,3 +5,18 @@ echo
 echo "Installing extra-special stuff for moovement"
 
 brew install z
+
+echo
+echo "Installing yarn"
+
+npm install -g yarn
+
+echo
+echo "Installing aws cli"
+
+brew install awscli
+
+echo
+echo "Installing serverless cli"
+
+yarn global add serverless
