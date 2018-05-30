d8f0b7e02f569c7a3bd84a2ff45d6747c74809d3
Daniel Garnier-Moiroux <dgarnier-moiroux@pivotal.io>
2018-05-30 14:11:52 +0200

    add aws config and credentials placeholder
    Signed-off-by: Anton Perez <aperez@mobiquityinc.com>

--- scripts/moovement/customizations.sh
+++ scripts/moovement/customizations.sh
@@ -19,3 +19,9 @@ echo
 echo "Copying bash config"
 
 cp $FILES_DIR/moovement.bash $HOME/.bash_it/custom
+
+echo
+echo "Copying aws credentials and config"
+
+if [ -z $(cat $HOME/.aws/credentials) ]; then cp $FILES_DIR/aws/credentials $HOME/.aws/credentials; fi
+if [ -z $(cat $HOME/.aws/config) ]; then cp $FILES_DIR/aws/config $HOME/.aws/config; fi
