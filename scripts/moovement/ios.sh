38cd5510ce2ead41bcff4a4c0b0ba5103b0250af
Mostafa Raafat <mraafat@mobiquityinc.com>
2018-05-30 15:06:52 +0200

    Add ios command-line tools
    Signed-off-by: Tim Jarratt <tjarratt@pivotal.io>

---
+++ scripts/moovement/ios.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+echo
+echo "Installing extra-special iOS tools"
+
+if ! type "swiftlint" > /dev/null; then
+  brew install swiftlint
+fi
+
+if ! type "carthage" > /dev/null; then
+  brew install carthage
+fi
+
+if ! type "rswift" > /dev/null; then
+  curl -L https://github.com/mac-cain13/R.swift/releases/download/v4.0.0/rswift-4.0.0.zip > /tmp/rswift.zip
+  unzip /tmp/rswift.zip -d /tmp
+  mv /tmp/rswift /usr/local/bin/rswift
+fi
+
--- scripts/moovement/setup.sh
+++ scripts/moovement/setup.sh
@@ -6,3 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source $SCRIPT_DIR/install.sh
 source $SCRIPT_DIR/customizations.sh
+source $SCRIPT_DIR/ios.sh
+
