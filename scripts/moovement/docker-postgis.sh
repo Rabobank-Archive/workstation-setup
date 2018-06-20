381033e74566f2d448b1fbda987a918995088508
Mostafa Raafat <mraafat@mobiquityinc.com>
2018-06-20 16:17:21 +0200

    Remove deprecated script
    Refer to new documentation in raboinnohub/moovement
    
    That explains how to setup postgres for local development
    
    Signed-off-by: Tim Jarratt <tjarratt@pivotal.io>

--- scripts/moovement/docker-postgis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-open /Applications/Docker.app
-docker pull mdillon/postgis
-
-docker run --name moovement-postgis -e POSTGRES_DB=moovement_db -d -p 5432:5432 mdillon/postgis
